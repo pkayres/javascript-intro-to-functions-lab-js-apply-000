c16e13bdde6e15c4190fdb1820f886707661ccc1
Pete A <peter.ayresmusic@gmail.com>
2019-01-25 00:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-    // if (string.toLowerCase()){
-    //   return "I can't hear you!"
-    // }
-    if (string.toUpperCase()) {
+    if (string === string.toLowerCase()){
+      return "I can't hear you!"
+    }
+    else if (string === string.toUpperCase()) {
       return 'YES INDEED!';
-    } else {
+    } else (string === "I love you, Grandma.")
       return "I love you, too."
     }
 
